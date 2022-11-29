12716b9666fa5eb58445053f66df1be7d42f007a
Rahul Ramesh <77114806+r4hu7@users.noreply.github.com>
2022-11-29 23:31:16 +0530

    producer consumer library

--- myKafka.py
+++ myKafka.py
@@ -8,6 +8,7 @@ class KafkaProducer():
     def __init__(self, bootstrap_servers):
         self.bootstrap_servers = bootstrap_servers
 
+        # connect to zookeeper
         HOST, PORT = self.bootstrap_servers[0].split(':')
         PORT = int(PORT)
 
@@ -16,10 +17,25 @@ class KafkaProducer():
         
         self.conn = sock
 
+        self.conn.send("Producer".encode('utf-8'))
+
+        # Get broker host and port from zookeeper
+        data = sock.recv(1024)
+        self.conn.close()
+
+        # Connect to broker
+        host, port = data.decode('utf-8').split(':')
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((HOST, PORT))
+        
+        self.conn = sock
+
+        self.conn.send("Producer".encode('utf-8'))
+
         print('Kafka Producer has been initiated...')
 
     def send(self, topic, value=None):
-        self.conn.send(value)
+        self.conn.send(f'{topic}:{value}'.encode('utf-8'))
 
         return self.conn.recv(1024)
 
@@ -31,6 +47,7 @@ class KafkaConsumer():
         self.auto_offset_reset = auto_offset_reset
         self.value_deserializer = value_deserializer
 
+        # connect to zookeeper
         HOST, PORT = self.bootstrap_servers[0].split(':')
         PORT = int(PORT)
 
@@ -38,7 +55,21 @@ class KafkaConsumer():
         sock.connect((HOST, PORT))
 
         self.conn = sock
+        self.conn.send("Consumer".encode('utf-8'))
         self.conn.send(topicName.encode('utf-8'))
+
+        # Get broker host and port from zookeeper
+        data = sock.recv(1024)
+        self.conn.close()
+        host, port = data.decode('utf-8').split(':')
+        
+        # connect to broker
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((host, int(port)))
+
+        self.conn = sock
+
+        self.conn.send("Consumer".encode('utf-8'))
 
         print('Kafka Consumer has been initiated...')
 
@@ -47,7 +78,7 @@ class KafkaConsumer():
             
             if not data:
                 break
-            
+
             print('Kafka Consumer has received a message from ' + str(self.bootstrap_servers) + ': ' + data.decode('utf-8'))
 
             if data.decode('utf-8') != '':
