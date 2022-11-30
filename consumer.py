c7e22377d61ca44ce5b73ce34950ee1e03055578
Pooshpal Baheti <pooshpalbaheti@gmail.com>
2022-11-30 12:43:19 +0530

    consumer.py ready

--- consumer.py
+++ consumer.py
@@ -1,60 +1,30 @@
-import sys
-from json import dumps
-
+import os.path
+import json
 from myKafka import KafkaConsumer
 
-bootstrap_servers = ['localhost:9092']
-topicName = sys.argv[1]
-consumer = KafkaConsumer (
-    topicName,
-    bootstrap_servers = bootstrap_servers,
-    auto_offset_reset = 'earliest',
-    value_deserializer = lambda x: x.decode('utf-8')
-)
+while True:
+    topicName = input("\nEnter topicName consumer subscribes to :")
+    oldData = []
 
-data = []
-result = {}
-states = []
+    try:
+        while True:
+            ## - send this topic to broker for every iteration
 
-try:
-    for line in consumer:
-        line = line.value
-        
-        if line == "EOF\n":
-            break
+            # Broker sends yes/no to consumer.
+            ack = [True,False] #recieve this value
 
-        else:
-            try:
-                state = line.split(',', 1)[0]
-                _, min_, max_, _ = line.replace('\n', '').rsplit(',', 3)
-                
-                data.append([state, min_, max_])
-                states.append(state)
-            except Exception as e:
+            topicLocation = str("./topic/"+topicName+".json")
+
+            if os.path.isfile(topicLocation) == True and ack == True:
+                f = open('data.json')
+                temp = json.load(f)
+                data = temp["val"]
+                for topic in list(set(temp) - set(oldData)):
+                    print(topic)        
+
+            else:
+                # print("No new data for :",topicName)
                 pass
 
-    states = [*set(states)]
-    states.sort()
-
-    for state in states:
-        
-        min_sum = max_sum = n = 0
-        
-        for line in data:
-            if line[0] == state:
-                n += 1
-                min_sum += float(line[1])
-                max_sum += float(line[2])
-
-        min_avg = round(min_sum/n, 2)
-        max_avg = round(max_sum/n, 2)
-
-        result[state] = {
-            "Min":min_avg,
-            "Max":max_avg
-        }
-        
-    print(dumps(result, indent=4), end="")
-
-except KeyboardInterrupt:
-    sys.exit()
+    except KeyboardInterrupt:
+        print('Unsubscribed!')
