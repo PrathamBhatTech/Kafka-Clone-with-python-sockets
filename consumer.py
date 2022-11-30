8387e92d741a77d0c2eed46218d5270f3fff73bf
Pooshpal Baheti <pooshpalbaheti@gmail.com>
2022-11-30 12:45:21 +0530

    unsubscribe

--- consumer.py
+++ consumer.py
@@ -2,29 +2,23 @@ import os.path
 import json
 from myKafka import KafkaConsumer
 
-while True:
-    topicName = input("\nEnter topicName consumer subscribes to :")
-    oldData = []
 
-    try:
-        while True:
-            ## - send this topic to broker for every iteration
-
-            # Broker sends yes/no to consumer.
-            ack = [True,False] #recieve this value
-
-            topicLocation = str("./topic/"+topicName+".json")
-
-            if os.path.isfile(topicLocation) == True and ack == True:
-                f = open('data.json')
-                temp = json.load(f)
-                data = temp["val"]
-                for topic in list(set(temp) - set(oldData)):
-                    print(topic)        
-
-            else:
-                # print("No new data for :",topicName)
-                pass
-
-    except KeyboardInterrupt:
-        print('Unsubscribed!')
+topicName = input("\nEnter topicName consumer subscribes to :")
+oldData = []
+try:
+    while True:
+        ## - send this topic to broker for every iteration
+        # Broker sends yes/no to consumer.
+        ack = [True,False] #recieve this value
+        topicLocation = str("./topic/"+topicName+".json")
+        if os.path.isfile(topicLocation) == True and ack == True:
+            f = open('data.json')
+            temp = json.load(f)
+            data = temp["val"]
+            for topic in list(set(temp) - set(oldData)):
+                print(topic)        
+        else:
+            # print("No new data for :",topicName)
+            pass
+except KeyboardInterrupt:
+    print('Unsubscribed!')
